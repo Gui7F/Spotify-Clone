28533335af96257749a9922818f7c4995f4fcc73
Guilherme Ribeiro <80535297+Gui7F@users.noreply.github.com>
2025-02-15 20:31:32 -0300

    Deploying Process

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,24 +1,36 @@
 import express from "express"
 import {db} from "./connect.js"
 import cors from "cors"
+import path from "path"
+
+const __dirname = path.resolve()
+
+
 const app = express()
 const PORT = 3001
 
 
 app.use(cors())
 
-app.get(`/`, (req, res) => {
-  res.send ("TEST")
-})
 
-app.get(`/artists`, async (req, res) => {
+
+app.get(`/api/apiartists`, async (req, res) => {
   res.send(await db.collection("artists").find({}).toArray())
 })
 
-app.get(`/songs`, async (req, res) => {
+app.get(`/api/apisongs`, async (req, res) => {
     res.send(await db.collection("songs").find({}).toArray())
+})
+
+app.use(express.static(path.join("../front-end/dist")))
+
+
+app.get("*", async (req, res) =>{
+    res.sendFile(path.join("../front-end/dist/index.html"))
 })
 
 app.listen(PORT, () => {
     console.log(`Servidor ouvindo na port : ${PORT}`)
 })
+
+
